d209de210cd18eb78e3328958df6c41b77a3d6f4
ernkny <eren.kinay.98@windowslive.com>
2022-10-04 12:10:37 +0300

    FactoryMethod DP added for Loging

---
+++ SecondHandCarBidProject.Logs/Abstract/ILogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondHandCarBidProject.Log.Abstract
+{
+    public interface ILogger<T>
+    {
+         Task DataLog(T data);
+
+    }
+}
---
+++ SecondHandCarBidProject.Logs/Concrete/MongoDatabaseLog.cs
@@ -0,0 +1,21 @@
+﻿using SecondHandCarBidProject.Log.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondHandCarBidProject.Log.Concrete
+{
+    public class MongoDatabaseLog<T> : ILogger<T> where T : class
+    {
+        public MongoDatabaseLog()
+        {
+
+        }
+        public async Task DataLog(T data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
