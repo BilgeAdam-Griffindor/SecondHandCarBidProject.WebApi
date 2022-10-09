82d9fd49d485e8dca28c9d7b6a0e51bda1f1c058
Endalth <alican__karamaras@hotmail.com>
2022-10-09 17:15:46 +0300

    Token creation for email verification and forgot password operations and mongo connection for these operations

---
+++ SecondHandCarBidProject.DataAccess/Mongo/Abstract/IMongoEmailPasswordToken.cs
@@ -0,0 +1,20 @@
+﻿using SecondHandCarBidProject.Common.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondHandCarBidProject.DataAccess.Mongo.Abstract
+{
+    public interface IMongoEmailPasswordToken
+    {
+        Task<string> AddToken(Guid userId, string tokenFor);
+
+        Task<GetTokenInfoDTO> GetTokenInfo(string token);
+
+        Task<bool> RemoveToken(string tokenId);
+
+        Task<bool> RemoveExpiredTokens();
+    }
+}
---
+++ SecondHandCarBidProject.DataAccess/Mongo/MongoModels/EmailPasswordTokenModel.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondHandCarBidProject.DataAccess.Mongo.MongoModels
+{
+    [BsonIgnoreExtraElements]
+    public class EmailPasswordTokenModel
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("user_id")]
+        public Guid UserId { get; set; }
+
+        [BsonElement("token")]
+        public string Token { get; set; }
+
+        [BsonElement("token_for")]
+        public string TokenFor { get; set; }
+
+        [BsonElement("expiration_date")]
+        public DateTime ExpirationDate { get; set; }
+    }
+}
